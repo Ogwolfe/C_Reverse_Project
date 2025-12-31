0da55570116dcca052dee4c39a71881f8e3ee7e6
Jason Wolfe <jason06051998@gmail.com>
2025-12-31 15:51:05 -0600

    Reworked the logic for differing argc counts

---
+++ readme.txt
@@ -0,0 +1,12 @@
+Use: reverse some stream of text
+
+Example: 
+
+    input.txt:  Hello, World!
+                This is an Example
+    
+    reverse input.txt:  This is an Example
+                        Hello, World!
+
+So the file is broken up into "lines". A "line" is essentially a sequence of chars that ends with a newline or EOF.
+So 
--- reverse.c
+++ reverse.c
@@ -2,39 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct Node {
+    char *string;
+    struct Node *next;
+};
+
+
+
 int main(int argc, char** argv){
 
-    FILE *input = NULL, *output = NULL;
+    FILE *input = stdin, *output = stdout;
 
+    //Error: Too many args
     if(argc > 3){
         fprintf(stderr, "usage: reverse <input> <output>\n");
         exit(1);
     }
 
-    if(argc == 3){
-        if(strcmp(argv[1], argv[2]) == 0){
-            fprintf(stderr, "Input and output file must differ\n");
-            exit(1);
-        }
-
+    //Input file provided
+    if(argc > 1){
         input = fopen(argv[1], "r");
         if(input == NULL){ 
             fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
+            exit(1);
+        }
+    }
+
+    //Output file also provided
+    if(argc > 2){
+        if(strcmp(argv[1], argv[2]) == 0){
+            fprintf(stderr, "Input and output file must differ\n");
+            fclose(input);
             exit(1);
         }
 
         output = fopen(argv[2], "w");
         if(output == NULL){
             fprintf(stderr, "error: cannot open file '%s'\n", argv[2]);
+            fclose(input);
             exit(1);
         }
-
-        fclose(input);
-        fclose(output);
     }
 
-
-
-    
+    fclose(input);
+    fclose(output);
     return 0;
 }
